4e1853deb80662b1ed00c7976069e01817e09019
JackNUMBER <antoine.cdrt@gmail.com>
2020-09-17 01:20:08 +0200

    extension start when Calimoto app is ready

---
+++ src/main.js
@@ -0,0 +1,16 @@
+window.CalimotoEnhancer = {
+
+  matches: false,
+
+  // from Material-Freebox-OS (by chteuchteu)
+  // https://github.com/chteuchteu/Material-Freebox-OS/blob/7ac105488671e4f40eb82f443f755488f275dd20/data/js/material-freebox-os.js#L140
+  environment: {
+    isFirefox: function() {
+      return /firefox/i.test(navigator.userAgent);
+    },
+
+    getBrowserHandle: function() {
+      return this.isFirefox() ? browser : chrome;
+    }
+  }
+}
---
+++ src/page_action/page_action.js
@@ -0,0 +1,46 @@
+const reactAppSelector = '.PlannerComponent';
+const browserHandle = CalimotoEnhancer.environment.getBrowserHandle();
+
+let reactAppContainer;
+
+const init = () => {
+  console.log('🌮 Calimoto Enhancer started');
+
+  reactAppContainer = document.querySelector(reactAppSelector);
+  CalimotoEnhancer.matches = true;
+}
+
+const timeout = () => {
+  console.error('Calimoto Enhancer waited 10 seconds and the Calimoto Trip Planner can\'t be found.');
+}
+
+/**
+ * Wait the React app loaded
+ * @param {string} selector the targeted selector we waiting for
+ * @param {int} timeoutInSeconds time before considering the app is too long loading
+ */
+function waitForAppReady(timeoutInSeconds = 10) {
+  return new Promise((resolve, reject) => {
+    let waited = 0;
+
+    const wait = (interval) => {
+      setTimeout(() => {
+        waited += interval;
+
+        if (!!document.querySelector(reactAppSelector)) {
+          return resolve()
+        }
+
+        if (waited >= timeoutInSeconds * 1000) {
+          return reject()
+        }
+
+        wait(interval)
+      }, interval)
+    };
+
+    wait(100);
+  })
+}
+
+const promise = waitForAppReady().then(init, timeout);
