a3fa3cb562343a44aaa5252a9f90a1120c1192bf
JackNUMBER <antoine.cdrt@gmail.com>
2024-05-08 22:31:23 +0200

    refactor const.js | fix #18

--- src/const.js
+++ src/const.js
@@ -1,9 +1,4 @@
 'use strict';
 
-window.CalimotoPoweride = {
-  options: ['smallerFont', 'biggerSearchbar', 'markerLinks'],
-  getBrowser: () => (/firefox/i.test(navigator.userAgent) ? browser : chrome),
-};
-
-const browserHandle = CalimotoPoweride.getBrowser();
-const optionsItems = CalimotoPoweride.options;
+const browserHandle = /firefox/i.test(navigator.userAgent) ? browser : chrome;
+const optionsItems = ['smallerFont', 'biggerSearchbar', 'markerLinks'];
